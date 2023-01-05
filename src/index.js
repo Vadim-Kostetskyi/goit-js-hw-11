28a9c5709b164c1bc2a951107afc1233d25fb54e
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-05 14:42:25 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
-import Notiflix from 'notiflix';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 import card from './teamplates/card.hbs';
 
 import { imgApi } from './find_pic_on_serwer.js';
@@ -12,37 +12,37 @@ const butt = document.getElementsByTagName('button')[0];
 const loadButt = document.querySelector('.load-more');
 
 let imageName = input.value;
-let pageNumber = 1;
+// let pageNumber = 1;
+const galery = new SimpleLightbox('.gallery__item');
 
 const imgAapi = new imgApi();
 
 async function getImg(imageName) {
-  await imgAapi
-    .fetchPhotos(imageName)
-    .then(response => {
-      if (response.data.totalHits === 0) {
-        throw new Error();
-      } else if (response.data.hits.length === 0) {
-        Notiflix.Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-      loadButt.classList.remove('hide');
-
-      imageBox.insertAdjacentHTML('beforeend', card(response.data.hits));
-
-      new SimpleLightbox('.gallery__item');
-    })
-    .catch(err =>
+  const response = await imgAapi.fetchPhotos(imageName);
+  try {
+    if (response.data.totalHits === 0) {
+      throw new Error();
+    } else if (response.data.hits.length === 0) {
       Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      )
+        "We're sorry, but you've reached the end of search results.",
+        loadButt.classList.add('hide')
+      );
+    } else {
+      loadButt.classList.remove('hide');
+      imageBox.insertAdjacentHTML('beforeend', card(response.data.hits));
+    }
+    galery.refresh();
+  } catch (err) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
     );
+  }
 }
 
 function inputImage(el) {
   el.preventDefault();
   imageName = el.target.searchQuery.value.trim();
+  imgAapi.pageNumber = 1;
 
   if (imageName.length === 0) {
     return Notiflix.Notify.failure(
@@ -52,13 +52,14 @@ function inputImage(el) {
 
   imageBox.innerHTML = '';
 
-  pageNumber = 1;
   getImg(imageName);
 }
 
 function addPhotos() {
+  loadButt.classList.add('hide');
   imgAapi.pageNumber += 1;
   getImg(imageName);
+  galery.refresh();
 }
 
 form.addEventListener('submit', inputImage);
